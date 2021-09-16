6d6a5fd623b924685c5e7a5cff5236d56e4eb9f5
Towhidul1999 <towhidulsislam@gmail.com>
2021-09-16 16:13:40 +0600

    movie-list page done

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './listItem.scss';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import AddIcon from '@material-ui/icons/Add';
 import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
 import ThumbDownOutlinedIcon from '@material-ui/icons/ThumbDownOutlined';
 
-export default function ListItem() {
+export default function ListItem({index}) {
+    const [isHovered, setIsHovered] = useState(false);
+    const trailer = "https://www.youtube.com/embed/uTrCgrA034A?controls=0";
+
     return (
-        <div className='listItem'>
+        <div className='listItem'
+        style={{left: isHovered && index * 225 - 50 + index * 2.5}}
+        onMouseEnter={ () => setIsHovered(true)} 
+        onMouseLeave={ () => setIsHovered(false)}
+        >
+
             <img src='https://www.themoviedb.org/t/p/w780/hVo9Gr69bDqTjZ4vEEo646TYhjm.jpg' alt=''/>
+
+            {isHovered && (
+
+        <>    
+            {/* <video src={trailer} autoPlay={true} loop/> */}
+            <iframe src={trailer} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
             <div className="itemInfo">
                 <div className="icons">
-                    <PlayArrowIcon/>
-                    <AddIcon/>
-                    <ThumbUpAltOutlinedIcon/>
-                    <ThumbDownOutlinedIcon/>
+                    <PlayArrowIcon className="icon"/>
+                    <AddIcon className="icon"/>
+                    <ThumbUpAltOutlinedIcon className="icon"/>
+                    <ThumbDownOutlinedIcon className="icon"/>
                 </div>
+                <div className="itemInfoTop">
+                    <span>1 hour 14 mins</span>
+                    <span className="limit">+16</span>
+                    <span>1999</span>
+                </div>
+                <div className="desc">
+                    is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+                </div>
+                <div className="genre">Action</div>
             </div>
+        </>
+            )};
         </div>
     )
 }
